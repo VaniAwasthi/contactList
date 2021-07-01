e36bf56e8d9932652f7d8bb0212631ab07701bb0
Vani Awasthi <awasthivani95@gmail.com>
2021-07-01 11:48:23 +0530

    ContactList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import List from "./Component/List";
+import axios from "axios";
+
+const BASE_URL = "https://dummyapi.io/data/api";
+const APP_ID = "60dc553ebaab3896ab8540f1";
 
 function App() {
+  const [contacts, setContacts] = useState({ data: [] });
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${BASE_URL}/user?limit=5`, { headers: { "app-id": APP_ID } })
+      .then(({ data }) => setContacts(data))
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <List loading={loading} contacts={contacts} />
+      <Footer />
     </div>
   );
 }
-
 export default App;
